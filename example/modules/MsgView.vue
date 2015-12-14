ba293c0870955fae40055421fc5e92deeb907ac2
BelinChung <belinchung@gmail.com>
2015-12-14 15:08:38 +0800

    调整目录结构

---
+++ example/modules/MsgView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page msg">
+    <Msg></Msg>
+  </div>
+</template>
+
+<script>
+import Msg from '../../src/msg/Msg.vue'
+
+export default {
+  name:'MsgView',
+  components:{
+    Msg
+  }
+}
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack')
 
 module.exports = {
   //页面入口文件配置
-  entry: './src/main.js',
+  entry: './example/main.js',
   //入口文件输出配置
   output: {
     path: './static',
